82b7dd47aee5801cb1bde820d9e966df07ea3f4b
alexey Shumakov <bobshaftoe@yandex.ru>
2016-04-21 10:58:12 +0500

    Fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,15 @@ user = User.create(email: 'test@mail.com', password: 'password', password_confir
 categories.each do |category|
   cat = Category.create(title: category)
   3.times do
-    post = Post.create(user: user, title: Faker::Hipster.sentence, body: Faker::Hipster.paragraph(10), sinopsis: Faker::Hipster.paragraph, category: cat)
+    post_params = {
+      user: user,
+      title: Faker::Hipster.sentence,
+      body: Faker::Hipster.paragraph(10),
+      avatar: File.new("#{Rails.root}/public/images/post/medium/missing.png"),
+      sinopsis: Faker::Hipster.paragraph,
+      category: cat
+    }
+    post = Post.create(post_params)
     5.times { Comment.create user: user, post: post, body: Faker::Hipster.paragraph }
   end
 end
