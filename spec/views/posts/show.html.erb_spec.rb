5a57e25ec4bb3ffe2833e06bd0b903a40e9edf66
alexey Shumakov <bobshaftoe@yandex.ru>
2016-04-29 13:36:47 +0500

    test posts#show view

--- spec/views/posts/show.html.erb_spec.rb
+++ spec/views/posts/show.html.erb_spec.rb
@@ -1,4 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe "posts/show.html.erb", type: :view do
+  let!(:moderator) { create :major_moderator }
+  let!(:post_model) { create :post }
+  let!(:comment) { create :comment, post: post_model, user: moderator }
+
+  before(:each) do
+    assign(:post, post_model)
+  end
+  describe 'show post_model fields' do
+    it 'show info' do
+      render
+      expect(rendered).to match(post_model.title)
+      expect(rendered).to match(post_model.sinopsis)
+      expect(rendered).to match(post_model.body)
+      expect(rendered).to match(post_model.impressions.to_s)
+    end
+
+    it 'show comments' do
+      render
+      expect(rendered).to match(post_model.comments.count.to_s)
+      expect(rendered).to match(comment.body)
+    end
+  end
 end
