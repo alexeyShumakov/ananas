42edb2d6960682ca995dec0404a6536a1d084004
alexey Shumakov <bobshaftoe@yandex.ru>
2016-05-18 18:09:46 +0500

    update email configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,8 +25,8 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.asset_host = 'http://ananas-journal.ru:3000'
-  config.action_mailer.default_url_options = { host: 'ananas-journal.ru', port: 3000 }
+  config.action_mailer.asset_host = 'http://ananas-journal.ru'
+  config.action_mailer.default_url_options = { host: 'ananas-journal.ru' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :user_name => ENV['SMPT_NAME'],
