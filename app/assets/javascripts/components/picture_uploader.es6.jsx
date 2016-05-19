888fbb6fba3e509d6e3906e8d1834b3669bc2c0d
alexey Shumakov <bobshaftoe@yandex.ru>
2016-05-19 13:32:22 +0500

    views fix

--- app/assets/javascripts/components/picture_uploader.es6.jsx
+++ app/assets/javascripts/components/picture_uploader.es6.jsx
@@ -25,18 +25,18 @@ class PictureUploader extends React.Component {
   render () {
     let urlBox;
     if (this.state.url) {
-      urlBox = 
+      urlBox =
         <div>
           <hr/>
           <p>
-            <b>Путь к вашей картинке:</b> {this.state.url}
+            <b>Путь к вашему изображению:</b> {this.state.url}
           </p>
-            Как пользоваться? вставьте в ваш пост: ![image]({this.state.url})
+            Как пользоваться? Просто добавьте: ![image]({this.state.url})
         </div>
     }
     return(
       <div className="panel panel-default">
-        <div className="panel-heading">Загрузить картинку</div>
+        <div className="panel-heading">Загрузить изображение</div>
         <div className="panel-body">
           <input type='file' onChange={this.handleChange.bind(this)}/>
           {urlBox}
