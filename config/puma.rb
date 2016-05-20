db0704d05c5b3ecaec9d31be244417d14f29630f
alexey Shumakov <bobshaftoe@yandex.ru>
2016-05-20 18:12:24 +0500

    update puma config

--- config/puma.rb
+++ config/puma.rb
@@ -1,10 +1,12 @@
-application_path = File.expand_path("../../", __FILE__)
-directory application_path
-environment ENV['RAILS_ENV'] || 'production'
-pidfile "#{application_path}/tmp/pids/puma.pid"
-stdout_redirect "#{application_path}/tmp/log/stdout", "#{application_path}/tmp/log/stderr"
-threads 2, 16
-workers 2
-bind 'tcp://0.0.0.0:9292'
-daemonize true
-preload_app!
+if ENV['RAILS_ENV'] == 'production'
+  application_path = ENV['PWD']
+  directory application_path
+  environment ENV['RAILS_ENV']
+  pidfile "#{application_path}/tmp/pids/puma.pid"
+  stdout_redirect "#{application_path}/tmp/log/stdout", "#{application_path}/tmp/log/stderr"
+  threads 2, 16
+  workers 2
+  bind 'tcp://0.0.0.0:9292'
+  daemonize true
+  preload_app!
+end
