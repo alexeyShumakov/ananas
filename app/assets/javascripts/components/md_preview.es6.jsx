99e7fa5e18ab16066fe94ae817eaa6e4401ec2a1
alexey Shumakov <bobshaftoe@yandex.ru>
2016-06-05 17:40:02 +0500

    md prev, loading indicator

--- app/assets/javascripts/components/md_preview.es6.jsx
+++ app/assets/javascripts/components/md_preview.es6.jsx
@@ -1,16 +1,17 @@
 class MdPreview extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {text: ''};
+    this.state = {text: '', isLoading: false};
   }
 
   updateMd() {
     let _this = this;
     let elementId = this.props.element_id;
     let bodyValue = $(elementId).val();
+    this.setState({isLoading: true})
     $.post('/posts/md_preview', { text: bodyValue }).then(
       (mdText) => {
-        _this.setState({text: mdText.text});
+        _this.setState({text: mdText.text, isLoading: false});
       }
     )
   }
@@ -19,9 +20,14 @@ class MdPreview extends React.Component {
     return { __html: this.state.text };
   }
   render () {
+    let loadingIndicator;
+    let isLoading = this.state.isLoading;
+    if(isLoading) {
+      loadingIndicator = ' loading...'
+    }
     return(
       <div>
-        <button className='pull-right btn btn-primary' onClick={this.updateMd.bind(this)}>Показать/обновить</button>
+        <button className='pull-right btn btn-primary' onClick={this.updateMd.bind(this)} disabled={isLoading} >Показать/обновить {loadingIndicator}</button>
         <h2>Предпросмотр</h2>
         <hr/>
         <div dangerouslySetInnerHTML={this.rawMd()} className='post__preview'></div>
